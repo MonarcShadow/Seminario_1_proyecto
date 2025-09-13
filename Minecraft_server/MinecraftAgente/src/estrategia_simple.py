72187c59f37515d6e48e7861eb198a7c0ff29dab
Genesis <114115044+MatiasPerez16@users.noreply.github.com>
2025-09-13 02:14:55 -0300

    Delete Minecraft_server/MinecraftAgente directory

--- Minecraft_server/MinecraftAgente/src/estrategia_simple.py
+++
@@ -1,4 +0,0 @@
-# src/estrategia_simple.py
-def mover_linea(inicio, pasos=5):
-    x, y, z = inicio
-    return [(x+i, y, z) for i in range(1, pasos+1)]
--- Minecraft_server/MinecraftAgente/src/test.py
+++
@@ -1,4 +0,0 @@
-from mcpi.minecraft import Minecraft
-
-mc = Minecraft.create()  # Conecta al servidor (localhost:4711)
-mc.postToChat("¡Hola desde Python!")
